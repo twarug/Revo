ce8ab4697c8e8b0095e8a7acb4bfc57518cf5774
MetGang <patrick_stritch@wp.pl>
2019-03-22 20:30:38 +0100

    Added more info to log

--- Revo/include/Revo/Graphics/Backend.hpp
+++ Revo/include/Revo/Graphics/Backend.hpp
@@ -9,7 +9,7 @@
 #include <glad/glad.h>
 
 #if defined(RV_DEBUG)
-#   define RV_GFX_CALL(__f, ...) rv::impl::GfxCall(RV_HERE, #__f, __f, __VA_ARGS__)
+#   define RV_GFX_CALL(__f, ...) rv::impl::GfxCall(RV_HERE, #__f "(" #__VA_ARGS__ ")", __f, __VA_ARGS__)
 #else
 #   define RV_GFX_CALL(__f, ...) __f(__VA_ARGS__)
 #endif
